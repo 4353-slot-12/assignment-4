60d08f93ad3afa3d0ebd432070e20030ea572773
teetuu2 <teetuuwork@gmail.com>
2022-03-29 15:35:18 -0500

    Added quote.js under models that will connect frontend to backend and execute SQL statements (unfinished)

---
+++ src/backend/models/quote.js
@@ -0,0 +1,31 @@
+import client from "../db.js";
+import QuoteService from '../services/quote.js';
+
+async function insertQuote() {
+    try {
+        quote = QuoteService.insert(userId, data, profile)
+        await client.connect();
+        await client.query(`INSERT INTO FuelQuote (timeStamp, gallonsRequested, deliveryAddress, deliveryDate, suggestedPrice, totalPrice) VALUES (${quote.timeStamp}, ${quote.gallonsRequested}, ${quote.deliveryAddress}, ${quote.deliveryDate}, ${quote.suggestedPrice}, ${quote.totalPrice});`);
+        await client.end();
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+test("quote insert", () => {
+    const userId = "abc";
+    const data = {
+        gallonsRequested: 5,
+        deliveryDate: "2022-03-13"
+    };
+    const profile = new Profile(userId, "a", "b", "c", "d", "e", "f");
+    const quote = QuoteService.insert(userId, data, profile);
+    expect(quote).toHaveProperty("gallonsRequested")
+    expect(quote).toHaveProperty("deliveryDate")
+    expect(quote).toHaveProperty("deliveryAddress")
+    expect(quote).toHaveProperty("timeStamp")
+    expect(quote).toHaveProperty("suggestedPrice")
+    expect(quote).toHaveProperty("totalPrice")
+    expect(quote.suggestedPrice).toMatch(dollarsRegex);
+    expect(quote.totalPrice).toMatch(dollarsRegex);
+});
