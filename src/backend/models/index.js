b89debbafd7e62125e3b43045dc196e0853bd174
Hyperlynx <hjake123@gmail.com>
2022-03-28 19:41:31 -0500

    Add attempt at get_profile method

--- src/backend/models/index.js
+++ src/backend/models/index.js
@@ -1,1 +1,16 @@
 import client from "../db.js";
+
+function get_profile(userId){
+    client.connect(function(err) {
+        if(err) {
+          return console.error('could not connect to postgres', err);
+        }
+        client.query('SELECT * FROM userprofiles WHERE userId = ' + userId, function(err, result) {
+          if(err) {
+            return console.error('error running query', err);
+          }
+          console.log(result.rows);
+          client.end(); // Remember to do this!
+        });
+      });
+}
