fa3d751961f538e43e614beda05621f6eeb34e77
teetuu2 <teetuuwork@gmail.com>
2022-03-31 22:35:34 -0500

    Added function to services/quote.js to find quote history

--- src/backend/models/quote.js
+++ src/backend/models/quote.js
@@ -1,12 +1,23 @@
 import client from "../db.js";
 import QuoteService from '../services/quote.js';
 
-async function insertQuote() {
+async function insertQuote(userId, data, profile) {
     try {
         quote = QuoteService.insert(userId, data, profile)
         await client.connect();
         await client.query(`INSERT INTO FuelQuote (timeStamp, gallonsRequested, deliveryAddress, deliveryDate, suggestedPrice, totalPrice) VALUES (${quote.timeStamp}, ${quote.gallonsRequested}, ${quote.deliveryAddress}, ${quote.deliveryDate}, ${quote.suggestedPrice}, ${quote.totalPrice});`);
         await client.end();
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+async function getQuoteHistory() {
+    try {
+        await client.connect();
+        const data = await client.query(`SELECT * FROM FuelQuote`);
+        await client.end();
+        return data;
     } catch(err) {
         console.log(err);
     }
