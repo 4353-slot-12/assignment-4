7596d7c168f21dc32836da1143858756cfc0a639
Adil Iqbal <aiqbal85@gmail.com>
2022-04-01 23:53:16 -0500

    working login/logout/register

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -2,19 +2,19 @@ import client from "../db.js";
 
 export default class UserModel {
     static async create(id, username, hash, salt) {
+      const query = `INSERT INTO users VALUES ($1, $2, $3, $4)`;
       try {
-        const query = `INSERT INTO users VALUES ($1, $2, $3, $4)`;
         await client.query(query, [id, username, hash, salt]);
         console.log(`Inserted user ${username}, with ID "${id}".`);
       } catch (err) {
         console.error(`Error on query: ${query}`);
         console.error(err.stack || err);
-      }
+      } 
     }
 
     static async findByUsername(username) {
+      const query = `SELECT * FROM users WHERE username = $1 LIMIT 1;`;
       try {
-        const query = `SELECT * FROM users WHERE username = $1 LIMIT 1;`;
         const res = await client.query(query, [username]);
         console.log(`Query "${query}" returned: ${JSON.stringify(res.rows[0], null, 4)}`);
         return res.rows[0];
@@ -25,8 +25,8 @@ export default class UserModel {
     }
 
     static async findById(userId) {
+      const query = `SELECT * FROM users WHERE id = $1 LIMIT 1;`;
       try {
-        const query = `SELECT * FROM users WHERE id = $1 LIMIT 1;`;
         const res = await client.query(query, [userId]);
         console.log(`Query "${query}" returned: ${JSON.stringify(res.rows[0], null, 4)}`);
         return res.rows[0];
