8befeb986624147da87d2f49381dccb21c84cc9e
Hyperlynx <hjake123@gmail.com>
2022-04-26 19:13:36 -0500

    Make pricing module import stuff

--- src/backend/models/quote.js
+++ src/backend/models/quote.js
@@ -1,5 +1,4 @@
 import client from "../db.js";
-import QuoteService from '../services/quote.js';
 
 export default async function insertQuote(userId, quote, profile) {
     await client.query(`INSERT INTO fuelquote VALUES ($1, $2, $3, $4, $5, $6, $7);`, 
@@ -10,21 +9,3 @@ export async function getQuoteHistory(userId) {
     const data = await client.query(`SELECT * FROM FuelQuote WHERE userid = $1`, [userId]);
     return data.rows;
 }
-
-// test("quote insert", () => {
-//     const userId = "abc";
-//     const data = {
-//         gallonsRequested: 5,
-//         deliveryDate: "2022-03-13"
-//     };
-//     const profile = new Profile(userId, "a", "b", "c", "d", "e", "f");
-//     const quote = QuoteService.insert(userId, data, profile);
-//     expect(quote).toHaveProperty("gallonsRequested")
-//     expect(quote).toHaveProperty("deliveryDate")
-//     expect(quote).toHaveProperty("deliveryAddress")
-//     expect(quote).toHaveProperty("timeStamp")
-//     expect(quote).toHaveProperty("suggestedPrice")
-//     expect(quote).toHaveProperty("totalPrice")
-//     expect(quote.suggestedPrice).toMatch(dollarsRegex);
-//     expect(quote.totalPrice).toMatch(dollarsRegex);
-// });
